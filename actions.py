be0bd5b76bc3dc6a451e014077548582e4599033
albertocasagrande99 <alberto.casagrande@studenti.unitn.it>
2022-12-17 19:02:21 +0100

    Database connection and pizza questions
    Item price and pizza availability
    Synonyms management

--- actions.py
+++ actions.py
@@ -11,15 +11,75 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+import sqlite3
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
 
 class ActionMenu(Action):
 	def name(self):
 		return 'action_menu'
 
 	def run(self, dispatcher, tracker, domain):
-		dispatcher.utter_message("These are the available pizzas")
+		conn = create_connection("pizzas.db")
+		cur = conn.cursor()
+		cur.execute("""SELECT title FROM pizzas """)
+		rows = cur.fetchall()
+		if len(list(rows)) < 1:
+			dispatcher.utter_message("These are no available pizzas at the moment")
+		else:
+			pizzas = ""
+			for i in rows:
+				pizzas = pizzas + i[0] + ", "
+			pizzas = pizzas[:-2]
+			dispatcher.utter_message("The available pizzas are: " + pizzas)
 		return[]
 
+class ActionPrice(Action):
+	def name(self):
+		return 'action_price'
+
+	def run(self, dispatcher, tracker, domain):
+		pizza_type = tracker.get_slot("pizza_type")
+		if(pizza_type!=None):
+			pizza_type = pizza_type.lower()
+		conn = create_connection("pizzas.db")
+		cur = conn.cursor()
+		cur.execute(f"""SELECT price FROM pizzas WHERE title='{pizza_type}'""")
+		rows = cur.fetchall()
+		if len(list(rows)) < 1:
+			dispatcher.utter_message(f"The pizza '{pizza_type}' is not present in the menu")
+		else:
+			dispatcher.utter_message("The price of " + pizza_type + " is " + rows[0][0] + " euros")
+		return[]
+
+class ActionPizzaAvailability(Action):
+	def name(self):
+		return 'action_pizza_availability'
+
+	def run(self, dispatcher, tracker, domain):
+		pizza_type = tracker.get_slot("pizza_type")
+		if(pizza_type!=None):
+			pizza_type = pizza_type.lower()
+		conn = create_connection("pizzas.db")
+		cur = conn.cursor()
+		cur.execute(f"""SELECT title FROM pizzas WHERE EXISTS (SELECT title FROM pizzas WHERE title='{pizza_type}')""")
+		rows = cur.fetchall()
+		if len(list(rows)) < 1:
+			dispatcher.utter_message(f"The pizza '{pizza_type}' is not present in the menu")
+		else:
+			dispatcher.utter_message("Yes! The "+pizza_type+" pizza is available.")
+		return[]
 
 class ActionChangeOrder(Action):
 	def name(self):
